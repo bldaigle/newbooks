fa0064cef0e3f882c889bb02af3a02004f36ddb6
Ben Daigle <bldaigle@owu.edu>
2016-03-18 16:09:33 -0400

    Changed to https

--- js/getNewBooks.js
+++ js/getNewBooks.js
@@ -4,7 +4,7 @@ function getNewBooks(url, container) {
 		$.ajax({
 	    	url: document.location.protocol + '//query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D\'' + encodeURIComponent(url) + '\'&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=',
 	    	beforeSend: function() {
-	      		$(container).html('<div class="loading"><img srce="http://bendaigle.ohio5.org/dev/newbooks/img/loader.gif" /></div>');
+	      		$(container).html('<div class="loading"><img srce="https://bendaigle.ohio5.org/dev/newbooks/img/loader.gif" /></div>');
 	    	},
 	    	dataType: 'json',
 	    	success: function(data) {
@@ -13,7 +13,7 @@ function getNewBooks(url, container) {
 	      			var catalogLink = value.link;
 	      			var bookTitle = value.title;
 	      			var shortTitle = $.trim(bookTitle).substring(0, 75) + '...';
-	      			var defaultCover = '<img src="http://bendaigle.ohio5.org/dev/newbooks/img/book-icon.png" />'
+	      			var defaultCover = '<img src="https://bendaigle.ohio5.org/dev/newbooks/img/book-icon.png" />'
 	      			var thehtml = '<div class="coverArt"><a href='+catalogLink+'>'+defaultCover+'</a><div class="bookTitle"><a href="'+catalogLink+'">'+shortTitle+'</a></div>'+covers+'</div>';
 	        		$(container).append(thehtml);
 	      		});
