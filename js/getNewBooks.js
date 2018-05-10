bb19fba1e16473e928157dd221ee3adf10fd1303
Benjamin Daigle <bldaigle@Benjamins-iMac.local>
2018-05-10 17:07:31 -0400

    Removed old commented code

--- js/getNewBooks.js
+++ js/getNewBooks.js
@@ -6,54 +6,30 @@ function getNewBooks(url, container) {
 	var getBooks = 
 		$.ajax({
 	    	url: document.location.protocol + '//api.rss2json.com/v1/api.json?count=100&rss_url=' + encodeURIComponent(url),
-	    	beforeSend: function() {
-	      		$(container).html('<div class="loading"><img srce="https://bendaigle.ohio5.org/dev/newbooks/img/loader.gif" /></div>');
-	    	},
 	    	dataType: 'json',
 	    	success: function(data) {
 	      		$.each(data.items, function(key, value) {
 	      			$('.lSSlideOuter a').attr('target','_blank');
-	      			var bookTitle = value.title;
-	      			var shortTitle = $.trim(bookTitle).substring(0,75) + '...';
-	      			var cover = value.thumbnail;
-	      			var catalogLink = value.link;
-	      			var defaultCover = '<img src="https://bendaigle.ohio5.org/dev/newbooks/img/book-icon.png" alt="' + bookTitle + '" />';
-
-	      			if (cover.length > 0) {
+	      			var bookTitle = value.title; // Get the title of the book
+	      			var shortTitle = $.trim(bookTitle).substring(0,75) + '...'; // Create a snippet from the title to display over default cover art image when no cover art is available.
+	      			var cover = value.thumbnail; // Get the cover art image
+	      			var catalogLink = value.link; // Get the catalog link
+	      			var defaultCover = '<img src="https://bendaigle.ohio5.org/dev/newbooks/img/book-icon.png" alt="' + bookTitle + '" />'; // Establish a default cover image to use when no art is avialable.
+	      			if (cover.length > 0) { // If there is cover art available, the use it and provide a link to the catalog record.
 	      				var thehtml = '<div class="coverArt"><a href="' + catalogLink + '"><img src="' + cover + '" alt="' + bookTitle + '" /></a></div>'
-
-	      			} else {
+	      			} else { // If there is no cover art available, display a default cover image, a short title snippet, and link both to the catalog record.
 	      				var thehtml = '<div class="coverArt"><a href="' + catalogLink + '">' + defaultCover + '</a><div class="bookTitle"><a href="' + catalogLink + '">' + shortTitle + '</a></div></div>'
 	      			}
-	      			
-	      			// var thehtml = '<div class="coverArt"><a href="' + catalogLink + '">' + defaultCover + '</a><div class="bookTitle"><a href="' + catalogLink + '">' + shortTitle + '</a></div><a href="' + catalogLink +'"><img src="' + cover + '" alt="' + bookTitle + '"/></a></div>';
-	      			$(container).append(thehtml);
+	      			$(container).append(thehtml); // Move the HTML above into the #newbooks div
 	      		});
-
-	      		//$.each(data.items, function(key, value){
-	      		//	$('.lSSlideOuter a').attr('target','_blank');
-	      		//	var catalogLink = value.link;
-	      		//	var bookTitle = value.title;
-	      		//	var shortTitle = $.trim(bookTitle).substring(0, 75) + '...';
-	      		//	var covers = value.description;
-	      		//	var imgAttr = $(this.content).find('img').attr('alt');
-	      		//	console.log(imgAttr);
-	      		//	var defaultCover = '<img alt="' + bookTitle + '" src="https://bendaigle.ohio5.org/dev/newbooks/img/book-icon.png" />'
-	      		//	var thehtml = '<div class="coverArt"><a href='+catalogLink+'>'+defaultCover+'</a><div class="bookTitle"><a href="'+catalogLink+'">'+shortTitle+'</a></div>'+covers+'</div>';
-	        	//	$(container).append(thehtml);
-
-	      		//});
 	    	},
 	    	error: function() {
 	      		$(container).html('<p>Oops! Try that again in a few moments.</p>');
 	    	}
 	  	});
-
 	getBooks.done(function(){
 		$(container).lightSlider({
 			pager: false,
 		});
 	});
-
-	
 };
